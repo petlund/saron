0aca4e777b9a2336543dc1b1ea9c768670d9067e
peter <peter@pl-air>
2019-07-24 16:16:34 +0200

    Update templatefile

--- config/config_template.php
+++ config/config_template.php
@@ -3,29 +3,22 @@ date_default_timezone_set('Europe/Stockholm');
 
 define("FullNameOfCongregation", "Baptistförsamlingen Saron-Korskyrkan");
 define("ShortNameOfCongregation", "Korskyrkan");
-define("UrlOfRegistry", "medlem.korskyrkan.se");
+define("UrlOfRegistry", "saron.korskyrkan.se");
 define("UrlOfCongregation", "www.korskyrkan.se");
-define("NameOfRegistry", "Registret");
+define("NameOfRegistry", "Saron");
 define("EmailSysAdmin", "sysman@korskyrkan.se");
 
 // DATABASE CONFIG
-define("HOST", "localhost");     // The host you want to connect to.
-define("USER", "saron");    // The database username. 
-define("PASSWORD", "saron");    // The database password. 
-define("DATABASE", "saron");    // The database name.
+define("HOST", "hostname");         // The host you want to connect to.
+define("USER", "user");             // The database username. 
+define("PASSWORD", "password");     // The database password. 
+define("DATABASE", "database");     // The database name.
  
 // APPROVAL TEXT FOR GDPR AGREEMENT WITH THE MEMBERS
-define ("APPROVAL", "Jag godkänner att Baptistförsamlingen Saron Korskyrkan registerför uppgifter inom ovan nämda rubrikområden i elektroniskt register, Uppgifterna är endast till för internt bruk inom församlingen. Icke individbaserad statistik såsom antalet medlemmar kan distribueras efter godkännande av församlingsledningen.");
+define ("APPROVAL", "Jag godkänner ....");
+// this text replace all fields witch include personel data on delete/anonymous
+define("ANONYMOUS", "_Anonymiserad_");
 
-// RESOURCES FOR PDF AND GRAPH GENERATION.
-// https://tcpdf.org/ 
-define("TCPDF_PATH", "/opt/registry_res/tcpdf");
-// http://jpgraph.net/
-
-// Path to True Type Fonts for jpgraph
-define('TTF_DIR','/Library/Fonts/');
-
-// https://www.webhostinghero.com/wordpress-authentication-integration-with-php/
 // PATH TO wp-load.php in your wordpress installation
 define("WP_SARON_LOGIN", "wp-load.php");
 
@@ -36,4 +29,20 @@ define("SARON_ROLE_VIEWER", "viewer");
 define("SARON_DISPLAY_NAME_EDITOR", "Uppdaterare");
 define("SARON_DISPLAY_NAME_VIEWER", "Tittare");
 
+//Override OTP if true
+define("TEST_ENV", false);
+
+define("WEB_ROOT", "www/");
+define("WP_URI", "");
+define("SARON_URI", "saron/");
+//root for Saron
+define("SARON_ROOT", WEB_ROOT . SARON_URI);
+//Root for Wordpress
+define("WP_ROOT", WEB_ROOT . WP_URI);
+// Path to Private key 
+define ("PKEY_FILE", "file:////" . SARON_ROOT . "cert/server.key");
+
+// RESOURCES FOR PDF GENERATION.
+// https://tcpdf.org/ 
+define("TCPDF_PATH", WEB_ROOT . SARON_URI . "tcpdf");
 
