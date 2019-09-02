c9ebb8c66e0fd8269d3568ea4a1035b8770dddf7
peter <peter@pl-air>
2019-09-02 20:59:51 +0200

    Mobilphone list bugfix tablename

--- app/views/MobileNumber.php
+++ app/views/MobileNumber.php
@@ -12,7 +12,7 @@
 
     $db = new db();
     $sql ="Select " . DECRYPTED_FIRSTNAME_LASTNAME_AS_NAME . ", ";
-    $sql.= DECRYPTED_ALIAS_MOBILE . " FROM PEOPLE WHERE " . SQL_WHERE_MEMBER . " and " . DECRYPTED_MOBILE . " is not null and "; 
+    $sql.= DECRYPTED_ALIAS_MOBILE . " FROM People WHERE " . SQL_WHERE_MEMBER . " and " . DECRYPTED_MOBILE . " is not null and "; 
     $sql.= "(Select count(*) from People as p where people.HomeId=p.HomeId and " . DECRYPTED_EMAIL . " like '%@%')  = 0 ";
 
 ?>
