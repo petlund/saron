8978660394147d88e1105a2a4abf9364a339ffc1
peter <peter@pl-air>
2019-09-02 20:40:54 +0200

    Person oobject

--- app/entities/Person.php
+++ app/entities/Person.php
@@ -12,5 +12,131 @@
  * @author peter
  */
 class Person {
-    //put your code here
+    private $HomeId;
+    private $LastName;
+    private $FirstName;
+    private $DateOfBirth;
+    private $Gender;
+    private $Email;
+    private $Mobile;
+    private $DateOfMembershipStart;
+    private $MembershipNo;
+    private $VisibleInCalendar;
+    private $Comment;
+       
+    function __construct() {
+        $this->HomeId = (int)filter_input(INPUT_POST, "HomeId", FILTER_SANITIZE_NUMBER_INT);
+        $this->LastName = (String)filter_input(INPUT_POST, "LastName", FILTER_SANITIZE_STRING);
+        $this->FirstName = (String)filter_input(INPUT_POST, "FirstName", FILTER_SANITIZE_STRING);
+        $this->DateOfBirth = (String)filter_input(INPUT_POST, "DateOfBirth", FILTER_SANITIZE_STRING);
+        $this->Gender = (int)filter_input(INPUT_POST, "Gender", FILTER_SANITIZE_NUMBER_INT);
+        $this->Email = (String)filter_input(INPUT_POST, "Email", FILTER_SANITIZE_EMAIL);
+        $this->Mobile = (String)filter_input(INPUT_POST, "Mobile", FILTER_SANITIZE_STRING);
+        $this->DateOfMembershipStart = (String)filter_input(INPUT_POST, "DateOfMembershipStart", FILTER_SANITIZE_STRING);
+        $this->MembershipNo = (int)filter_input(INPUT_POST, "MembershipNo", FILTER_SANITIZE_NUMBER_INT);
+        $this->VisibleInCalendar = (int)filter_input(INPUT_POST, "VisibleInCalendar", FILTER_SANITIZE_NUMBER_INT);    
+        $this->Comment = (String)filter_input(INPUT_POST, "Comment", FILTER_SANITIZE_STRING);
+    }
+    
+    function read(){
+        return;
+    }
+
+    function checkData(){
+        $error = array();
+        $error["Result"] = "ERROR";
+        if(strlen($this->FirstName)==0 or strlen($this->LastName)==0 or strlen($this->DateOfBirth)==0){
+            $error["Message"] = "Personen behöver ett för- och ett efternamn samt ett födelsedadum för att kunna lagras i registret";
+        }
+
+        if($this->MembershipNo < 1 and strlen($this->this->DateOfMembershipStart)!==0){
+            $error["Message"] = "Personen har ett datum för start av medlemskap men saknar medlemsnummer. Lägg till ett medlemsnummer.";
+        }
+
+        if($this->VisibleInCalendar === 0 and strlen($this->DateOfMembershipStart)!==0){
+            $error["Message"] = "Ange om personen ska vara synlig i adresskalendern eller ej.";
+        }
+
+        if($this->VisibleInCalendar === 2){
+            if((strlen($this->DateOfMembershipStart)===0 and strlen($this->DateOfMembershipEnd)===0) or strlen($this->DateOfMembershipEnd)!==0){
+                $error["Message"] = "Icke medlemmar ska inte vara synliga i adresskalendern.";
+            }
+        }
+
+        if($this->MembershipNo > 0 and strlen($this->DateOfMembershipStart)===0){
+            $error["Message"] = "Personen har ett inget datum för start av medlemskap men har ett medlemsnummer. Lägg till ett datum för start av medlemskap.";
+        }
+        if(strlen($error["Message"])>0){
+            return json_encode($error);
+        }
+        else{
+            return true;
+        }
+    }
+    
+    function insert(){
+        $this->sqlInsert = "INSERT INTO People (LastNameEncrypt, FirstNameEncrypt, DateOfBirth, Gender, EmailEncrypt, MobileEncrypt, DateOfMembershipStart, MembershipNo, VisibleInCalendar, CommentEncrypt, Inserter, HomeId) ";
+        $this->sqlInsert.= "VALUES (";
+        $this->sqlInsert.= "AES_ENCRYPT('" . salt() . $this->LastName . "', " . PKEY . "), ";
+        $this->sqlInsert.= "AES_ENCRYPT('" . salt() . $this->FirstName . "', " . PKEY . "), ";
+        $this->sqlInsert.= "'" . $this->DateOfBirth . "', ";
+        $this->sqlInsert.= "" . $this->Gender . ", ";
+        
+        if(strlen($this->Email)>0){
+            $this->sqlInsert.= "AES_ENCRYPT('" . salt() . $this->Email . "', " . PKEY . "), ";
+        }
+        else{
+            $this->sqlInsert.= "null, ";                    
+        }
+        
+        if(strlen($this->Mobile)>0){
+            $this->sqlInsert.= "AES_ENCRYPT('" . salt() . $this->Mobile . "', " . PKEY . "), ";
+        }
+        else{
+            $this->sqlInsert.= "null, ";                    
+        }
+        
+        if(strlen($this->DateOfMembershipStart)>0){    
+            $this->sqlInsert.= "'" . $this->DateOfMembershipStart . "', ";
+        } 
+        else{ 
+            $this->sqlInsert.= "null, ";        
+        }
+
+        if($this->MembershipNo===0){
+            $this->sqlInsert.= "null, ";
+        }
+        else{
+            $this->sqlInsert.= $this->MembershipNo . ", ";
+        }    
+
+        $this->sqlInsert.= $this->VisibleInCalendar . ", ";
+        
+        if(strlen($this->Comment)>0){
+            $this->sqlInsert.= "AES_ENCRYPT('" . salt() . $this->Comment . "', " . PKEY . "), ";
+        }
+        else{
+            $this->sqlInsert.= "null, ";                    
+        }
+        
+        $this->sqlInsert.= $this->id . ", ";
+
+
+        
+        return;
+    }
+
+    function read(){
+        return;
+    }
+    
+    function update(){
+        return;
+    }
+    
+    function delete(){
+        return;
+    }
+    
+    
 }
--- app/entities/createPerson.php
+++ app/entities/createPerson.php
@@ -6,6 +6,7 @@ require_once 'config.php';
 require_once SARON_ROOT . "app/access/wp-authenticate.php";
 require_once SARON_ROOT . 'app/database/queries.php'; 
 require_once SARON_ROOT . 'app/database/db.php';
+require_once SARON_ROOT . 'app/entities/Person.php';
 
 /*** REQUIRE USER AUTHENTICATION ***/
     $requireEditorRole = true;
@@ -20,107 +21,14 @@ require_once SARON_ROOT . 'app/database/db.php';
             $id = (int) $user->ID;
         }
 
-        $HomeId = (int)filter_input(INPUT_POST, "HomeId", FILTER_SANITIZE_NUMBER_INT);
-        $LastName = (String)filter_input(INPUT_POST, "LastName", FILTER_SANITIZE_STRING);
-        $FirstName = (String)filter_input(INPUT_POST, "FirstName", FILTER_SANITIZE_STRING);
-        $DateOfBirth = (String)filter_input(INPUT_POST, "DateOfBirth", FILTER_SANITIZE_STRING);
-        $Gender = (int)filter_input(INPUT_POST, "Gender", FILTER_SANITIZE_NUMBER_INT);
-        $Email = (String)filter_input(INPUT_POST, "Email", FILTER_SANITIZE_EMAIL);
-        $Mobile = (String)filter_input(INPUT_POST, "Mobile", FILTER_SANITIZE_STRING);
-        $DateOfMembershipStart = (String)filter_input(INPUT_POST, "DateOfMembershipStart", FILTER_SANITIZE_STRING);
-        $MembershipNo = (int)filter_input(INPUT_POST, "MembershipNo", FILTER_SANITIZE_NUMBER_INT);
-        $VisibleInCalendar = (int)filter_input(INPUT_POST, "VisibleInCalendar", FILTER_SANITIZE_NUMBER_INT);    
-        $Comment = (String)filter_input(INPUT_POST, "Comment", FILTER_SANITIZE_STRING);
-
-        $sqlInsert = "INSERT INTO People (LastNameEncrypt, FirstNameEncrypt, DateOfBirth, Gender, EmailEncrypt, MobileEncrypt, DateOfMembershipStart, MembershipNo, VisibleInCalendar, CommentEncrypt, Inserter, HomeId) ";
-        $sqlInsert.= "VALUES (";
-        $sqlInsert.= "AES_ENCRYPT('" . salt() . $LastName . "', " . PKEY . "), ";
-        $sqlInsert.= "AES_ENCRYPT('" . salt() . $FirstName . "', " . PKEY . "), ";
-        $sqlInsert.= "'" . $DateOfBirth . "', ";
-        $sqlInsert.= "" . $Gender . ", ";
-        
-        if(strlen($Email)>0){
-            $sqlInsert.= "AES_ENCRYPT('" . salt() . $Email . "', " . PKEY . "), ";
+        $person = new Person();
+        $checkResult=$person->checkData();
+        if($checkResult===true){
         }
         else{
-            $sqlInsert.= "null, ";                    
+            echo $checkResult;
         }
-        
-        if(strlen($Mobile)>0){
-            $sqlInsert.= "AES_ENCRYPT('" . salt() . $Mobile . "', " . PKEY . "), ";
-        }
-        else{
-            $sqlInsert.= "null, ";                    
-        }
-        
-        if(strlen($DateOfMembershipStart)>0){    
-            $sqlInsert.= "'" . $DateOfMembershipStart . "', ";
-        } 
-        else{ 
-            $sqlInsert.= "null, ";        
-        }
-
-        if($MembershipNo===0){
-            $sqlInsert.= "null, ";
-        }
-        else{
-            $sqlInsert.= $MembershipNo . ", ";
-        }    
-
-        $sqlInsert.= $VisibleInCalendar . ", ";
-        
-        if(strlen($Comment)>0){
-            $sqlInsert.= "AES_ENCRYPT('" . salt() . $Comment . "', " . PKEY . "), ";
-        }
-        else{
-            $sqlInsert.= "null, ";                    
-        }
-        
-        $sqlInsert.= $id . ", ";
-
-
-        if(strlen($FirstName)==0 or strlen($LastName)==0 or strlen($DateOfBirth)==0){
-            $error = array();
-            $error["Result"] = "ERROR";
-            $error["Message"] = "Personen behöver ett för- och ett efternamn samt ett födelsedadum för att kunna lagras i registret";
-            echo json_encode($error);
-            exit();
-        }
-
-        if($MembershipNo < 1 and strlen($DateOfMembershipStart)!==0){
-            $error = array();
-            $error["Result"] = "ERROR";
-            $error["Message"] = "Personen har ett datum för start av medlemskap men saknar medlemsnummer. Lägg till ett medlemsnummer.";
-            echo json_encode($error);
-            exit();
-        }
-
-        if($VisibleInCalendar === 0 and strlen($DateOfMembershipStart)!==0){
-            $error = array();
-            $error["Result"] = "ERROR";
-            $error["Message"] = "Ange om personen ska vara synlig i adresskalendern eller ej.";
-            echo json_encode($error);
-            exit();
-        }
-
-        if($VisibleInCalendar === 2){
-            if((strlen($DateOfMembershipStart)===0 and strlen($DateOfMembershipEnd)===0) or strlen($DateOfMembershipEnd)!==0){
-                $error = array();
-                $error["Result"] = "ERROR";
-                $error["Message"] = "Icke medlemmar ska inte vara synliga i adresskalendern.";
-                echo json_encode($error);
-                exit();
-            }
-        }
-
-        if($MembershipNo > 0 and strlen($DateOfMembershipStart)===0){
-            $error = array();
-            $error["Result"] = "ERROR";
-            $error["Message"] = "Personen har ett inget datum för start av medlemskap men har ett medlemsnummer. Lägg till ett datum för start av medlemskap.";
-            echo json_encode($error);
-            exit();
-        }
-
+exit();
         try{
             $db = new db();            
             $db->transaction_begin();
@@ -138,11 +46,11 @@ require_once SARON_ROOT . 'app/database/db.php';
             }
 
             $sqlInsert.= ")";
-            $select = SQL_STAR_PEOPLE . ", ". DECRYPTED_FIRSTNAME_LASTNAME_AS_NAME . ", " . ADDRESS_ALIAS_LONG_HOMENAME . ", " . DATES_AS_ALISAS_MEMBERSTATES;     
+            $sql = SQL_STAR_PEOPLE . ", ". DECRYPTED_FIRSTNAME_LASTNAME_AS_NAME . ", " . ADDRESS_ALIAS_LONG_HOMENAME . ", " . DATES_AS_ALISAS_MEMBERSTATES;     
             
             $NewPersonId = $db->insert($sqlInsert, "People", "Id");
 
-            $result = $db->select($user, $select, SQL_FROM_PEOPLE_LEFT_JOIN_HOMES, "Where People.Id = " . $NewPersonId, "", "", "Record");
+            $result = $db->select($user, $sql, SQL_FROM_PEOPLE_LEFT_JOIN_HOMES, "Where People.Id = " . $NewPersonId, "", "", "Record");
 
             $db->transaction_end();
             echo $result;     
