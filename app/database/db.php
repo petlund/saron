cc844034d6514be73a29da7ea4217456cb971ad9
peter <peter@pl-air>
2019-12-04 20:13:46 +0100

    Create dev error log for db

--- app/database/db.php
+++ app/database/db.php
@@ -42,6 +42,7 @@ class db {
     
     
     function transaction_roll_back(){
+        $this->php_dev_error_log("transaction_roll_back", "");
         return $this->connection->rollback();
     }
     
@@ -49,6 +50,7 @@ class db {
     public function delete($sqlDelete){
         if(!$listResult = $this->connection->query($sqlDelete)){
             $technicalErrMsg = $this->connection->errno . ": " . $this->connection->error;
+            $this->php_dev_error_log("delete", $sqlDelete);
             throw new Exception($this->jsonErrorMessage("SQL-Error in delete statement!", null, $technicalErrMsg));
         }
         else{
@@ -61,6 +63,7 @@ class db {
         $sql = $update . $set . $where;
         if(!$listResult = $this->connection->query($sql)){
             $technicalErrMsg = $this->connection->errno . ": " . $this->connection->error;
+            $this->php_dev_error_log("update", $sql);
             throw new Exception($this->jsonErrorMessage("SQL-Error in update statement!", null, $technicalErrMsg));
         }
         return true;
@@ -80,10 +83,12 @@ class db {
         $sqlCount = "Select count(*) as c from Homes WHERE Id = "  . $HomeId . ";";
         
         if(!$listResult = $this->connection->query($sql)){
+            $this->php_dev_error_log("selectHome", $sql);
             throw new Exception($this->jsonErrorMessage("SQL-Error in selectHome statement!", null, $this->connection->error));
         }
 
         if(!$countResult = $this->connection->query($sqlCount)){
+            $this->php_dev_error_log("selectHome Count ", $sqlCount);
             throw new Exception($this->jsonErrorMessage("SQL-Error in selectHomeCount statement!", null, $this->connection->error));
         }
         $arrayResult = $this->resultSetToArray($listResult);
@@ -96,12 +101,12 @@ class db {
         $sql.= "UPPER(CONVERT(BINARY " . DECRYPTED_FIRSTNAME . " USING utf8)) like '%" . $FirstName . "' and ";
         $sql.= "UPPER(CONVERT(BINARY " . DECRYPTED_LASTNAME . " USING utf8)) like '%" . $LastName . "' and ";
         $sql.= "DateOfBirth like '" . $DateOfBirth . "'";
-        error_log("SQL: " . $sql);
         if($Id>0){
             $sql.= " and ID <> '" . $Id . "'";            
         }
         
         if(!$listResult = $this->connection->query($sql)){
+            $this->php_dev_error_log("exist ", $sql);
             throw new Exception($this->jsonErrorMessage("SQL-Error in select /exist/ statement!", null, $sql));
         }
         $countRows = "0";
@@ -111,6 +116,7 @@ class db {
         mysqli_free_result($listResult);
         
         if($countRows !== "0"){
+            $this->php_dev_error_log("exist ", $FirstName . " " .  $LastName . " " . $DateOfBirth);
             $errorMsg = "En person med identitet:<br><b>" . $FirstName . " " . $LastName . " " . $DateOfBirth . "</b><br>finns redan i databasen.";
             throw new Exception($this->jsonErrorMessage($errorMsg));
         }       
@@ -137,6 +143,7 @@ class db {
             return $LastId;
         }
         catch(Exception $error){
+            $this->php_dev_error_log("insert", $insert);
             $technicalErrMsg = $this->connection->errno . ": " . $this->connection->error;
             throw new Exception($this->jsonErrorMessage("Error in insert statement!", null, $technicalErrMsg));
         }
@@ -151,6 +158,7 @@ class db {
             return $this->selectSeparate($user, $sqlSelect, $sqlCount, $responstype);
         }
         catch(Exception $error){
+            $this->php_dev_error_log("select", $select);
             throw new Exception($error->getMessage());
         }
     } 
@@ -158,11 +166,13 @@ class db {
     public function selectSeparate($user, $sqlSelect, $sqlCount, $responstype="Records"){
         if(!$listResult = $this->connection->query($sqlSelect)){
             $technicalErrMsg = $this->connection->errno . ": " . $this->connection->error;
+            $this->php_dev_error_log("selectSeparate 1 ", $sqlSelect);
             throw new Exception($this->jsonErrorMessage("SQL-Error in select /list/ statement!", null, $technicalErrMsg));
         }
 
         if(!$countResult = $this->connection->query($sqlCount)){
             $technicalErrMsg = $this->connection->errno . ": " . $this->connection->error;
+            $this->php_dev_error_log("selectSeparate 2 ", $sqlSelect);
             throw new Exception($this->jsonErrorMessage("SQL-Error in select count statement!", null, $technicalErrMsg));
         }
 
@@ -177,10 +187,11 @@ class db {
         if(!$listResult){
             $technicalErrMsg = $this->connection->errno . ": " . $this->connection->error;
             echo "SQL-Error in statement: <br>" .  $sql . "<br>" .  $technicalErrMsg; 
+            $this->php_dev_error_log("sqlQuery", $sql);
             return false;
         }
         $resultArray = $this->resultSetToArray($listResult);
-
+    
         return $resultArray;
     }
 
@@ -202,6 +213,7 @@ class db {
         $errMsg.= mb_convert_encoding("<br><br>Om problemet kvarstår kontakta systemadministratören<BR>", "UTF-8", "auto");
         $errMsg.= "<a href='mailto:" . EmailSysAdmin . "'>" . EmailSysAdmin . "</a>";
         $error["Message"] = $errMsg;
+        $this->php_dev_error_log("jsonErrorMessage", $errMsg);
         return json_encode($error);        
     }
         
@@ -237,8 +249,19 @@ class db {
         $jsonResult = json_encode($jTableResult);
         
         if($jsonResult===false){
+            $this->php_dev_error_log("processRowSet", "");
             throw new Exception($this->jsonErrorMessage("Error i json_encode funktionen!", null, " -- processRowSet"));
         }
         return $jsonResult;
     }    
+    
+    
+    
+    function php_dev_error_log($method, $msg){
+        if(TEST_ENV === true){
+            error_log("\n\nDB method " . $method . ": " . $msg);
+        }
+    }
+
+    
 }
