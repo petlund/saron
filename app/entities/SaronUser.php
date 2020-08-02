8fdea463e8931814ad31bdbea26df607f56d9f36
peter <peter@peters-air>
2020-08-02 22:34:15 +0200

    Bugfix saronuser control

--- app/entities/SaronUser.php
+++ app/entities/SaronUser.php
@@ -14,6 +14,7 @@
 require_once "config.php";
 
 class SaronUser {
+    private $NO_ROLE = "NO_ROLE";
     private $user;
     public $ID;
     function __construct($user) {
@@ -24,12 +25,10 @@ class SaronUser {
        
     function isSaronUser(){
         /*** Check if the user had a saron role ***/
-        for($i = 0; $i < count($this->user->roles); $i++){
-            if(substr($this->user->roles[$i], 0, strlen(SARON_ROLE_PREFIX)) === SARON_ROLE_PREFIX){ // CHECK IF THE USER IS A MEMBER OF THE GROUP  saron_edit
-                $otp = $this->user->get("wp-otp");
-                if($otp["enabled"] || TEST_ENV){ // In test environment OTP enabeled account is not necessary.
-                    return true;
-                }
+        if($this->getRole() !== $this->NO_ROLE){ 
+            $otp = $this->user->get("wp-otp");
+            if($otp["enabled"] || TEST_ENV){ // In test environment OTP enabeled account is not necessary.
+                return true;
             }
         }         
         return false;
@@ -45,12 +44,14 @@ class SaronUser {
         } 
         return false;
     }
+
     
     
     function getDisplayName(){
         return $this->user->user_firstname . " " . $this->user->user_lastname ;
     }
     
+
     
     function getRole(){
         for($i = 0; $i < count($this->user->roles); $i++){
@@ -63,32 +64,14 @@ class SaronUser {
                 return SARON_ROLE_VIEWER;                
             }
         }   
-        return "NO ROLE";
+        return NO_ROLE;
     }
     
 
     function getRoleSql($continue){
         $SQL_ALIAS = ' as user_role';
-        $userRole = "";
-        
-        for($i = 0; $i < count($this->user->roles); $i++){
-            if($this->user->roles[$i]===SARON_ROLE_PREFIX . SARON_ROLE_EDITOR){
-                $userRole = SARON_ROLE_EDITOR;                    
-            }
-        }
-        if(strlen($userRole) === 0){
-            for($i = 0; $i < count($this->user->roles); $i++){
-                if($this->user->roles[$i]===SARON_ROLE_PREFIX . SARON_ROLE_VIEWER){
-                    return "'" . SARON_ROLE_VIEWER . "' as user_role ";                    
-                }
-            }   
-        }
-        
-        if(strlen($userRole) === 0){
-            $userRole = "NO_ROLE";
-        }
             
-        $sql = "'" . $userRole . "'" . $SQL_ALIAS;
+        $sql = "'" . $this->getRole() . "'" . $SQL_ALIAS;
             
         if($continue){
             return $sql . ", ";
@@ -99,21 +82,24 @@ class SaronUser {
     }
     
 
+    
     function getRoleDisplayName(){
         for($i = 0; $i < count($this->user->roles); $i++){
-            if($this->user->roles[$i]===SARON_ROLE_PREFIX . SARON_ROLE_EDITOR){
+            if($this->user->roles[$i]==SARON_ROLE_PREFIX . SARON_ROLE_EDITOR){
                 return SARON_DISPLAY_NAME_EDITOR;
             }
         }
         for($i = 0; $i < count($this->user->roles); $i++){
-            if($this->user->roles[$i]===SARON_ROLE_PREFIX . SARON_ROLE_VIEWER){
+            if($this->user->roles[$i]==SARON_ROLE_PREFIX . SARON_ROLE_VIEWER){
                 return SARON_DISPLAY_NAME_VIEWER;                
             }
         }   
-        return "NO ROLE";
+        return NO_ROLE;
     }
     
+    
+    
     function getNameAndRole(){
-        return $this->getDisplayName() . " -  " . $this->getRoleDisplayName();
+        return $this->getDisplayName() . " - " . $this->getRoleDisplayName();
     }
 }
