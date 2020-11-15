ac445a928e1f38411df4c02ced4471ff20942ef4
peter <peter@peters-air>
2020-11-15 15:35:39 +0100

    RC 1.3.6 bugfix updating of sub role and sub unit table

--- app/entities/OrganizationRoleUnitType.php
+++ app/entities/OrganizationRoleUnitType.php
@@ -65,7 +65,7 @@ class OrganizationRoleUnitType extends SuperEntity{
         $select.= $this->saronUser->getRoleSql(false) . " ";
         $from = "FROM Org_Role as Role inner join `Org_Role-UnitType` as Rut on Rut.OrgRole_FK = Role.Id ";
         if($id > 0){
-            $where = "WHERE Role.Id= " . $id . " ";            
+            $where = "WHERE Rut.Id= " . $id . " ";            
         }
         else if($this->orgUnitType_FK > 0){
             $where = "WHERE Rut.OrgUnitType_FK = " . $this->orgUnitType_FK . " ";
@@ -84,7 +84,7 @@ class OrganizationRoleUnitType extends SuperEntity{
         $select.= $this->saronUser->getRoleSql(false) . " ";
         $from = "FROM Org_UnitType as Typ inner join `Org_Role-UnitType` as Rut on Rut.OrgUnitType_FK = Typ.Id ";
         if($id > 0){
-            $where = "WHERE Typ.Id= " . $id . " ";            
+            $where = "WHERE Rut.Id= " . $id . " ";            
         }
         else if($this->orgRole_FK > 0){
             $where = "WHERE Rut.OrgRole_FK = " . $this->orgRole_FK . " ";
