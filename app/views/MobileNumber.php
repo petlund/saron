3c032ae2a416e9285fbc239d85f51a59385b9e37
peter <peter@peters-air>
2020-11-18 19:29:03 +0100

    bugfix references

--- app/views/MobileNumber.php
+++ app/views/MobileNumber.php
@@ -3,10 +3,10 @@
     header("Pragma: no-cache"); //HTTP 1.0
     header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
     require_once 'config.php';
-    require_once SARON_ROOT . "/app/access/wp-authenticate.php";
+    require_once SARON_ROOT . "app/access/wp-authenticate.php";
     require_once SARON_ROOT . "menu.php";
-    require_once SARON_ROOT . '/app/database/queries.php';
-    require_once SARON_ROOT . '/app/database/db.php';
+    require_once SARON_ROOT . 'app/database/queries.php';
+    require_once SARON_ROOT . 'app/database/db.php';
 
     isLoggedIn();
 
