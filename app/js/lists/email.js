1581b89f83a3f4180ded9b87e98972d8f97f5952
peter <peter@peters-air>
2022-03-17 18:00:06 +0100

    Bufgix url parameters

--- app/js/lists/email.js
+++ app/js/lists/email.js
@@ -1,4 +1,4 @@
-/* global saron.uri.saron, DATE_FORMAT */
+/* global saron, DATE_FORMAT */
 "use strict";
 
 $(document).ready(function () {
@@ -8,17 +8,14 @@ $(document).ready(function () {
 
     
     
-    $.get( '/' + saron.uri.saron + 'app/web-api/listPeople.php?selection=email', function(text) {
+    $.get( '/' + saron.uri.saron + 'app/web-api/listPeople.php?ResultType=' + saron.responsetype.records + '&TableView=' + saron.list.email.viewid, function(text) {
         var data = JSON.parse(text);
         var cnt = data.TotalRecordCount;
         var head = '<div class="saronSmallText">Mailadresser att kopiera och klistra in i adressfält för hemlig kopia. (' + cnt + ' st.)</div><br>';
         var str = head;
-        for(var i = 0; i<cnt; i++){                
-            if(data.Records[i].Email !== null)
-                if(data.Records[i].Email.includes('@')){
-                    str = str + data.Records[i].Email + ', ';
-                }
-        }
+        for(var i = 0; i<cnt; i++)                
+            str = str + data.Records[i].Email + ', ';
+
         element.innerHTML = str;
     
     });
