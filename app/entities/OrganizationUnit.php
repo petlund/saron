792b8b7cb691397ee4ce04fadcd443a0c77eab66
peter <peter@peters-air>
2022-12-01 18:13:32 +0100

    Bugfix inhibit unit move to unit without child enabled

--- app/entities/OrganizationUnit.php
+++ app/entities/OrganizationUnit.php
@@ -59,15 +59,17 @@ class OrganizationUnit extends SuperEntity{
         case SOURCE_LIST:
             break;
         case SOURCE_EDIT:
-            $where = "WHERE Tree.Id <> " . $this->id . " ";    
+            If($this->field === 'ParentTreeNode_FK'){
+                $where = "WHERE NOT (Typ.SubUnitEnabled = 1 OR Tree.Id IN (" . $this->selectSubNodesSql($this->id) . ")) ";
+            }
+            else{
+                $where = "WHERE Tree.Id <> " . $this->id . " ";    
+            }
             break;
         default:
             $where = "";    
             break;
         }
-        If($this->filter === 'yes'){
-            $where = "WHERE NOT (Typ.SubUnitEnabled = 0 OR Tree.Id IN (" . $this->selectSubNodesSql($this->id) . ")) ";
-        }        
 
         $result = $this->db->select($this->saronUser, $select , $from, $where, "Order by DisplayText ", "", "Options");    
         return $result; 
