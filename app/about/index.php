81620f06139f9d523dce60e3d87608c7302007b7
peter <peter@peters-air>
2023-03-12 16:26:05 +0100

    Missing set new version number in about-file

--- app/about/index.php
+++ app/about/index.php
@@ -14,7 +14,7 @@
     </Head>
     <body> 
         <div class='saronSmallText'>Medlemsregister för församlingen i <?php echo ShortNameOfCongregation;
-        echo '<br>Version 2.0.0';
+        echo '<br>Version 2.1.1';
         echo '<br><br><a href="mailto:' . EmailSysAdmin . '">' . EmailSysAdmin . '</a>';
         ?></div>
     </body>
